99c3057f7f40e07e587a9868087380269920601c
ddlogesh <ddlogesh@gmail.com>
2019-06-30 18:14:34 +0530

    Login partial done

--- app/src/main/java/in/edu/ssn/ssnapp/LoginActivity.java
+++ app/src/main/java/in/edu/ssn/ssnapp/LoginActivity.java
@@ -1,19 +1,38 @@
 package in.edu.ssn.ssnapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.squareup.picasso.Picasso;
 
 public class LoginActivity extends BaseActivity {
 
     CardView signInCV;
     ImageView roadIV, skyIV, signBoardIV;
+    FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
+    private static int RC_SIGN_IN = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +42,69 @@ public class LoginActivity extends BaseActivity {
         signInCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build();
+
+                mGoogleSignInClient = GoogleSignIn.getClient(getApplicationContext(), gso);
+                signIn();
+
+                //startActivity(new Intent(getApplicationContext(),HomeActivity.class));
             }
         });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Log.d("test_set", e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("test_set", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("test_set", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            Log.d("test_set", "signInWithCredential:failure");
+                        }
+                    }
+                });
     }
 
     void initUI(){
         changeFont(regular,(ViewGroup) this.findViewById(android.R.id.content));
         signInCV = findViewById(R.id.signInCV);
+        mAuth = FirebaseAuth.getInstance();
         /*roadIV = findViewById(R.id.roadIV);
         skyIV = findViewById(R.id.skyIV);
         signBoardIV = findViewById(R.id.signBoardIV);*/
@@ -38,5 +112,6 @@ public class LoginActivity extends BaseActivity {
         Picasso.get().load("file:///android_asset/road_with_grass.png").into(roadIV);
         Picasso.get().load("file:///android_asset/sign_board.png").into(signBoardIV);*/
     }
-
 }
+
+//FirebaseAuth.getInstance().signOut();
