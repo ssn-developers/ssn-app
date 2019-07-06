015da0cbd4e6a15957d7f1ada682bfe8ed8a06f3
shrikanth7698 <catcalm7698@gmail.com>
2019-07-06 19:03:37 +0530

    Onboarding activity done

---
+++ app/src/main/java/in/edu/ssn/ssnapp/onboarding/OnboardingActivity.java
@@ -0,0 +1,128 @@
+package in.edu.ssn.ssnapp.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import in.edu.ssn.ssnapp.R;
+import in.edu.ssn.ssnapp.adapters.ViewPagerAdapter;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    ImageView backgroundIV,backgroundIV1,backgroundIV2;
+    public static boolean firstRun1=false;
+    public static boolean firstRun2=false;
+    public static boolean firstRun3=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        initUI();
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int positionOffsetPixels) {
+                if(v<0.70 && i==0 && !firstRun1){
+                    OneFragment.startAnimation();
+                    firstRun1=true;
+                }
+                if(v>0.30 && i==0 && !firstRun2){
+                    TwoFragment.startAnimation();
+                    firstRun2=true;
+                }
+                if(v<0.70 && i==1 && !firstRun2){
+                    TwoFragment.startAnimation();
+                    firstRun2=true;
+                }
+                if(v>0.40 && i==1 && !firstRun3){
+                    ThreeFragment.startAnimation();
+                    firstRun3=true;
+                }
+
+                switch (i){
+                    case 0:{
+                        backgroundIV.setRotation(v * 180.0f);
+                        backgroundIV.setAlpha(1-v);
+                        backgroundIV.setScaleX(1-v);
+                        backgroundIV.setScaleY(1-v);
+
+                        backgroundIV1.setRotation(v * 360f);
+                        backgroundIV1.setAlpha(v);
+                        backgroundIV1.setScaleX(v);
+                        backgroundIV1.setScaleY(v);
+
+                        break;
+                    }
+                    case 1:{
+                        backgroundIV1.setRotation(v * 180.0f);
+                        backgroundIV1.setAlpha(1-v);
+                        backgroundIV1.setScaleX(1-v);
+                        backgroundIV1.setScaleY(1-v);
+
+                        backgroundIV2.setRotation(v * 360f);
+                        backgroundIV2.setAlpha(v);
+                        backgroundIV2.setScaleX(v);
+                        backgroundIV2.setScaleY(v);
+
+                        break;
+                    }
+                    case 2:{
+                        backgroundIV2.setRotation(v * 180.0f);
+                        backgroundIV2.setAlpha(1-v);
+                        backgroundIV2.setScaleX(1-v);
+                        backgroundIV2.setScaleY(1-v);
+
+                        break;
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    void initUI(){
+        viewPager = findViewById(R.id.viewPager);
+        backgroundIV = findViewById(R.id.backgroundIV);
+        backgroundIV1 = findViewById(R.id.backgroundIV1);
+        backgroundIV2 = findViewById(R.id.backgroundIV2);
+        Picasso.get().load("file:///android_asset/onboarding/bg1.png").into(backgroundIV);
+        Picasso.get().load("file:///android_asset/onboarding/bg2.png").into(backgroundIV1);
+        Picasso.get().load("file:///android_asset/onboarding/bg3.png").into(backgroundIV2);
+        startAnimation();
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "OneFragment");
+        adapter.addFragment(new TwoFragment(), "TwoFragment");
+        adapter.addFragment(new ThreeFragment(),"ThreeFragment");
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(3);
+        //dotsIndicator.setViewPager(viewPager);
+        //dotsIndicator.setDotsClickable(false);
+    }
+
+    public void startAnimation(){
+        backgroundIV.animate().alpha(1).scaleY(1).scaleX(1).setDuration(600).start();
+    }
+}
---
+++ app/src/main/java/in/edu/ssn/ssnapp/onboarding/ThreeFragment.java
@@ -0,0 +1,109 @@
+package in.edu.ssn.ssnapp.onboarding;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import in.edu.ssn.ssnapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ThreeFragment extends Fragment {
+
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+    static ImageView hand1IV, hand2IV, hand3IV, landIV;
+    static ImageView tempIV;
+    static boolean firstRun=false;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_three, container, false);
+        initUI(view);
+        //startAnimation();
+        return view;
+    }
+
+    private void initUI(View view){
+        hand1IV = view.findViewById(R.id.hand1IV);
+        hand2IV = view.findViewById(R.id.hand2IV);
+        hand3IV = view.findViewById(R.id.hand3IV);
+        landIV = view.findViewById(R.id.landIV);
+        tempIV = view.findViewById(R.id.tempIV);
+
+        Picasso.get().load("file:///android_asset/onboarding/hand1.png").into(hand1IV);
+        Picasso.get().load("file:///android_asset/onboarding/hand2.png").into(hand2IV);
+        Picasso.get().load("file:///android_asset/onboarding/hand3.png").into(hand3IV);
+        Picasso.get().load("file:///android_asset/onboarding/land2.png").into(landIV);
+    }
+
+    public static void startAnimation(){
+        firstRun=true;
+        landIV.animate().translationY(tempIV.getBaseline()).scaleY(1).scaleX(1).setDuration(600);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hand1IV.animate()
+                        .translationY(tempIV.getBaseline())
+                        .scaleX(1)
+                        .scaleY(1)
+                        .setDuration(800)
+                        .setInterpolator(new AccelerateDecelerateInterpolator());
+                hand2IV.animate()
+                        .translationY(tempIV.getBaseline())
+                        .scaleX(1)
+                        .scaleY(1)
+                        .setDuration(800)
+                        .setStartDelay(150)
+                        .setInterpolator(new AccelerateDecelerateInterpolator());
+                hand3IV.animate()
+                        .translationY(tempIV.getBaseline())
+                        .scaleX(1)
+                        .scaleY(1)
+                        .setDuration(800)
+                        .setStartDelay(250)
+                        .setInterpolator(new AccelerateDecelerateInterpolator());
+
+            }
+        },200);
+
+    }
+
+    public static void clearAnimation(){
+        landIV.animate().translationY(landIV.getWidth()/2).scaleX(0).scaleY(0).setDuration(1);
+        hand1IV.animate().translationY(1000).scaleX(0).scaleY(0).setDuration(1);
+        hand2IV.animate().translationY(1000).scaleX(0).scaleY(0).setDuration(1);
+        hand3IV.animate().translationY(1000).scaleX(0).scaleY(0).setDuration(1);
+        OnboardingActivity.firstRun3=false;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if(isVisibleToUser){
+            /*if(firstRun){
+                startAnimation();
+            }*/
+        }else {
+            if(firstRun)
+                clearAnimation();
+        }
+    }
+
+}
