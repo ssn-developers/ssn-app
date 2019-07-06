72646ce3a8b0014a314beca6b9e5d04e502b7a66
harshavardhanp <harshavardhan.zodiac@gmail.com>
2019-07-06 16:35:59 +0530

    Added FCM push notification feature

--- app/src/main/java/in/edu/ssn/ssnapp/SplashActivity.java
+++ app/src/main/java/in/edu/ssn/ssnapp/SplashActivity.java
@@ -1,5 +1,6 @@
 package in.edu.ssn.ssnapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Handler;
@@ -9,17 +10,28 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
 import in.edu.ssn.ssnapp.models.BusRoute;
+import in.edu.ssn.ssnapp.models.Post;
+import in.edu.ssn.ssnapp.utils.Constants;
+import in.edu.ssn.ssnapp.utils.FCMHelper;
 import in.edu.ssn.ssnapp.utils.SharedPref;
 
 public class SplashActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
+    Intent intent;
+    private final static String TAG="SplashActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         db = FirebaseFirestore.getInstance();
+        intent=getIntent();
         //startActivity(new Intent(getApplicationContext(), HomeActivity.class));
         new updateFaculty().execute();
     }
@@ -68,9 +81,95 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     public void passIntent(){
-        if(SharedPref.getBoolean(getApplicationContext(),"is_logged_in"))
-            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        if(SharedPref.getBoolean(getApplicationContext(),"is_logged_in")){
+
+            handleIntent(intent);
+        }
         else
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
+
+
+
+    void handleIntent(Intent intent){
+        // handling the payload from the push notification
+        String postType=" ",postId="",pdfUrl="";
+
+        Bundle bundle=intent.getExtras();
+
+        if(bundle!=null){
+
+            if(bundle.containsKey("PostType")){
+                postType=intent.getStringExtra("PostType");
+            }
+
+            if(bundle.containsKey("PostId")){
+                postId=intent.getStringExtra("PostId");
+            }
+            if(bundle.containsKey("PdfUrl")){
+                pdfUrl=intent.getStringExtra("PdfUrl");
+            }
+
+            Log.d(TAG,"post details: "+postId+" "+postType);
+
+            if(postType.equals("1")){
+                FetchPostById(this,postId);
+            }
+            else if(postType.equals("2")){
+                intent=new Intent(this,PdfViewerActivity.class);
+                intent.putExtra(Constants.PDF_URL,pdfUrl);
+                startActivity(intent);
+            }
+
+        }
+        else {
+            Log.d(TAG,"no extras to handle");
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        }
+    }
+
+
+
+    public static void FetchPostById(final Context context, String postId){
+
+        DocumentReference documentReference = FirebaseFirestore.getInstance().collection("post").document(postId);
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot postSnapShot) {
+                Log.d(TAG,"post fetched successfully");
+                final Post post = new Post();
+                Log.d(TAG,postSnapShot.getString("title"));
+                post.setTitle(postSnapShot.getString("title"));
+                post.setDescription(postSnapShot.getString("description"));
+                post.setTime(postSnapShot.getTimestamp("time").toDate());
+
+                ArrayList<String> images = (ArrayList<String>) postSnapShot.get("img_urls");
+                if(images != null){
+                    post.setImageUrl(images);
+                }
+
+                String id = postSnapShot.getString("author");
+
+                post.setAuthor(SharedPref.getString(context,"faculty",id + "_name"));
+                post.setAuthor_image_url(SharedPref.getString(context,"faculty",id + "_dp_url"));
+                post.setPosition(SharedPref.getString(context,"faculty",id + "_position"));
+
+                Intent intent = new Intent(context, PostDetailsActivity.class);
+                intent.putExtra("post",post);
+                intent.putExtra("time",FCMHelper.getTime(post.getTime()));
+                context.startActivity(intent);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG,"failed to fetch the post");
+
+            }
+        });
+
+    }
+
+
+
 }
---
+++ app/src/main/java/in/edu/ssn/ssnapp/utils/FCMHelper.java
@@ -0,0 +1,215 @@
+package in.edu.ssn.ssnapp.utils;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.firebase.ui.firestore.SnapshotParser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import in.edu.ssn.ssnapp.HomeActivity;
+import in.edu.ssn.ssnapp.PostDetailsActivity;
+import in.edu.ssn.ssnapp.R;
+import in.edu.ssn.ssnapp.models.BusPost;
+import in.edu.ssn.ssnapp.models.Post;
+import in.edu.ssn.ssnapp.services.SSNFirebaseMessagingService;
+
+public class FCMHelper {
+
+    final static String TAG="FCMHelper";
+
+    public static String getToken(final Context context){
+
+        String token="null";
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        String token = task.getResult().getToken();
+                        Log.d(TAG, token);
+                        SharedPref.putString(context,"FCMToken",token);
+                        //UpdateFCM(context,token);
+                        //SubscribeToTopic(context,SharedPref.getString(context,"dept"));
+                    }
+                });
+
+        return token;
+    }
+
+    public static void UpdateFCM(final Context context,final String fcmToken){
+
+        String id=SharedPref.getString(context,"id");
+        FirebaseFirestore.getInstance().collection("user").document(id).update("FCMToken",fcmToken)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG,"Added the FCM token successfully");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG,"Failed to add the FCM token ");
+                    }
+                });
+    }
+
+    public static void SubscribeToTopic(final Context context,String topic){
+        FirebaseMessaging.getInstance().subscribeToTopic(topic)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "subscribe success";
+                        if (!task.isSuccessful()) {
+                            msg = "subscribe failed";
+                        }
+                        Log.d(TAG, msg);
+                        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    public static void UnSubscribeToTopic(final Context context, String topic){
+
+        FirebaseMessaging.getInstance().unsubscribeFromTopic(topic)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "unsubscribe success";
+                        if (!task.isSuccessful()) {
+                            msg = "unsubscribe failed";
+                        }
+                        Log.d(TAG, msg);
+                        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    public static void showNotification(String message, Context context, Intent intent){
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationManager notificationManager=(NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(new NotificationChannel("1","general",NotificationManager.IMPORTANCE_HIGH));
+            Notification.Builder nbuilder=new Notification.Builder(context,"1")
+                    .setContentTitle("hello")
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentText(message)
+                    .setChannelId("1")
+                    .setContentIntent(pendingIntent);
+
+            notificationManager.notify(1,nbuilder.build());
+
+        }else {
+            Notification.Builder nbuilder=new Notification.Builder(context)
+                    .setContentTitle("hello")
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentText(message+" from zodiac");
+
+            notificationManager.notify(1,nbuilder.build());
+        }
+
+    }
+
+
+    public static void FetchPostById(final Context context,String postId){
+
+        DocumentReference documentReference = FirebaseFirestore.getInstance().collection("post").document(postId);
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot postSnapShot) {
+                Log.d(TAG,"post fetched successfully");
+                final Post post = new Post();
+                Log.d(TAG,postSnapShot.getString("title"));
+                post.setTitle(postSnapShot.getString("title"));
+                post.setDescription(postSnapShot.getString("description"));
+                post.setTime(postSnapShot.getTimestamp("time").toDate());
+
+                ArrayList<String> images = (ArrayList<String>) postSnapShot.get("img_urls");
+                if(images != null){
+                    post.setImageUrl(images);
+                }
+
+                String id = postSnapShot.getString("author");
+
+                post.setAuthor(SharedPref.getString(context,"faculty",id + "_name"));
+                post.setAuthor_image_url(SharedPref.getString(context,"faculty",id + "_dp_url"));
+                post.setPosition(SharedPref.getString(context,"faculty",id + "_position"));
+
+
+                Intent intent = new Intent(context, PostDetailsActivity.class);
+                intent.putExtra("post",post);
+                intent.putExtra("time",getTime(post.getTime()));
+                FCMHelper.showNotification(post.getDescription(),context,intent);
+                Log.d(TAG,"success fetching the post of type 1");
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG,"failed to fetch the post");
+
+            }
+        });
+
+    }
+
+    public static String getTime(Date time){
+
+        Date now = new Date();
+        Long t = now.getTime() - time.getTime();
+
+        String diff_time;
+        if(t < 60000)
+            return Long.toString(t / 1000) + "s ago";
+        else if(t < 3600000)
+            return Long.toString(t / 60000) + "m ago";
+        else if(t < 86400000)
+            return Long.toString(t / 3600000) + "h ago";
+        else if(t < 604800000)
+            return Long.toString(t/86400000) + "d ago";
+        else if(t < 2592000000L)
+            return Long.toString(t/604800000) + "w ago";
+        else if(t < 31536000000L)
+            return Long.toString(t/2592000000L) + "M ago";
+        else
+            return Long.toString(t/31536000000L) + "y ago";
+    }
+
+
+}
