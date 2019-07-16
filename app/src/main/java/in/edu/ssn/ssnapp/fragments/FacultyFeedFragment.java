453d5b79eb57e5ff9c15ec2d2d4f6d14d518c8dc
Ezhil <ezhil99mutsun@gmail.com>
2019-07-16 09:08:02 +0530

    faculty login integrated feeds partially done

---
+++ app/src/main/java/in/edu/ssn/ssnapp/fragments/FacultyFeedFragment.java
@@ -0,0 +1,252 @@
+package in.edu.ssn.ssnapp.fragments;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.firebase.ui.firestore.SnapshotParser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.hendraanggrian.appcompat.widget.SocialTextView;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import in.edu.ssn.ssnapp.PostDetailsActivity;
+import in.edu.ssn.ssnapp.R;
+import in.edu.ssn.ssnapp.adapters.ImageAdapter;
+import in.edu.ssn.ssnapp.models.Post;
+import in.edu.ssn.ssnapp.utils.CommonUtils;
+import in.edu.ssn.ssnapp.utils.SharedPref;
+
+public class FacultyFeedFragment extends Fragment {
+
+    public FacultyFeedFragment() { }
+
+    RecyclerView feedsRV;
+    FirestoreRecyclerAdapter adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+        CommonUtils.initFonts(getContext(), view);
+        initUI(view);
+
+        setupFireStore();
+
+        return view;
+    }
+
+    /*********************************************************/
+
+    void setupFireStore(){
+        String id = SharedPref.getString(getContext(),"id");
+//        String year = "year." + Integer.toString(SharedPref.getInt(getContext(),"year"));
+
+        //TODO: Needs to manually create composite query before release for each author. [VERY IMPORTANT]
+
+        Query query = FirebaseFirestore.getInstance().collection("post").whereEqualTo("author","dummyid1").orderBy("time", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Post> options = new FirestoreRecyclerOptions.Builder<Post>().setQuery(query, new SnapshotParser<Post>() {
+            @NonNull
+            @Override
+            public Post parseSnapshot(@NonNull DocumentSnapshot snapshot) {
+                final Post post = new Post();
+                post.setId(snapshot.getString("id"));
+                post.setTitle(snapshot.getString("title"));
+                post.setDescription(snapshot.getString("description"));
+                post.setTime(snapshot.getTimestamp("time").toDate());
+
+                ArrayList<String> images = (ArrayList<String>) snapshot.get("img_urls");
+                if(images != null && images.size() != 0)
+                    post.setImageUrl(images);
+                else {
+                    post.setImageUrl(null);
+                }
+
+                String id = snapshot.getString("author");
+
+                post.setAuthor(SharedPref.getString(getContext(),"faculty",id + "_name"));
+                post.setAuthor_image_url(SharedPref.getString(getContext(),"faculty",id + "_dp_url"));
+                post.setPosition(SharedPref.getString(getContext(),"faculty",id + "_position"));
+
+                return post;
+            }
+        })
+        .build();
+
+        adapter = new FirestoreRecyclerAdapter<Post, FeedViewHolder>(options) {
+            @Override
+            public void onBindViewHolder(final FeedViewHolder holder, final int position, final Post model) {
+                holder.tv_author.setText(model.getAuthor());
+
+                try {
+                    if (model.getAuthor_image_url() != null)
+                        Picasso.get().load(model.getAuthor_image_url()).placeholder(R.drawable.ic_user_white).into(holder.userImageIV);
+                    else
+                        holder.userImageIV.setImageResource(R.drawable.ic_user_white);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    holder.userImageIV.setImageResource(R.drawable.ic_user_white);
+                }
+
+                holder.tv_position.setText(model.getPosition());
+                holder.tv_title.setText(model.getTitle());
+
+                Date time = model.getTime();
+                Date now = new Date();
+                Long t = now.getTime() - time.getTime();
+                String timer;
+
+                if(t < 60000)
+                    timer = Long.toString(t / 1000) + "s ago";
+                else if(t < 3600000)
+                    timer = Long.toString(t / 60000) + "m ago";
+                else if(t < 86400000)
+                    timer = Long.toString(t / 3600000) + "h ago";
+                else if(t < 604800000)
+                    timer = Long.toString(t/86400000) + "d ago";
+                else if(t < 2592000000L)
+                    timer = Long.toString(t/604800000) + "w ago";
+                else if(t < 31536000000L)
+                    timer = Long.toString(t/2592000000L) + "M ago";
+                else
+                    timer = Long.toString(t/31536000000L) + "y ago";
+
+                holder.tv_time.setText(timer);
+
+                if(model.getDescription().length() > 100) {
+                    SpannableString ss = new SpannableString(model.getDescription().substring(0, 100) + "... see more");
+                    ss.setSpan(new RelativeSizeSpan(0.9f), ss.length() - 12, ss.length(), 0);
+                    ss.setSpan(new ForegroundColorSpan(Color.parseColor("#404040")), ss.length() - 12, ss.length(), 0);
+                    holder.tv_description.setText(ss);
+                }
+                else
+                    holder.tv_description.setText(model.getDescription().trim());
+
+                if(model.getImageUrl()!=null && model.getImageUrl().size() != 0) {
+                    holder.viewPager.setVisibility(View.VISIBLE);
+
+                    final ImageAdapter imageAdapter = new ImageAdapter(getContext(), model.getImageUrl(),true, model, timer);
+                    holder.viewPager.setAdapter(imageAdapter);
+
+                    if(model.getImageUrl().size()==1){
+                        holder.tv_current_image.setVisibility(View.GONE);
+                    }else {
+                        holder.tv_current_image.setVisibility(View.VISIBLE);
+                        holder.tv_current_image.setText(String.valueOf(1)+" / "+String.valueOf(model.getImageUrl().size()));
+                        holder.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                            @Override
+                            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                            }
+
+                            @Override
+                            public void onPageSelected(int pos) {
+                                holder.tv_current_image.setText(String.valueOf(pos+1)+" / "+String.valueOf(model.getImageUrl().size()));
+                            }
+
+                            @Override
+                            public void onPageScrollStateChanged(int state) {
+
+                            }
+                        });
+                    }
+                }
+                else {
+                    holder.viewPager.setVisibility(View.GONE);
+                    holder.tv_current_image.setVisibility(View.GONE);
+                }
+
+                holder.feed_view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getContext(), PostDetailsActivity.class);
+                        intent.putExtra("post", model);
+                        intent.putExtra("time", holder.tv_time.getText().toString());
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public FeedViewHolder onCreateViewHolder(ViewGroup group, int i) {
+                View view = LayoutInflater.from(group.getContext()).inflate(R.layout.post_item, group, false);
+                return new FeedViewHolder(view);
+            }
+        };
+
+        feedsRV.setAdapter(adapter);
+    }
+
+    void initUI(View view){
+        feedsRV = view.findViewById(R.id.feedsRV);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        feedsRV.setLayoutManager(layoutManager);
+        feedsRV.setHasFixedSize(true);
+    }
+
+    /*********************************************************/
+
+    public class FeedViewHolder extends RecyclerView.ViewHolder {
+        public TextView tv_author, tv_position, tv_title, tv_time, tv_current_image;
+        public SocialTextView tv_description;
+        public ImageView userImageIV;
+        public RelativeLayout feed_view;
+        public ViewPager viewPager;
+
+        public FeedViewHolder(View itemView) {
+            super(itemView);
+
+            tv_author = itemView.findViewById(R.id.tv_author);
+            tv_position = itemView.findViewById(R.id.tv_position);
+            tv_title = itemView.findViewById(R.id.tv_title);
+            tv_description = itemView.findViewById(R.id.tv_description);
+            tv_time = itemView.findViewById(R.id.tv_time);
+            tv_current_image = itemView.findViewById(R.id.currentImageTV);
+            userImageIV = itemView.findViewById(R.id.userImageIV);
+            feed_view = itemView.findViewById(R.id.feed_view);
+            viewPager = itemView.findViewById(R.id.viewPager);
+
+        }
+    }
+
+    /*********************************************************/
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        adapter.stopListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+}
