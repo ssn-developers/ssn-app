75cc243ca6da509761998e159e22456b20e25225
ddlogesh <ddlogesh@gmail.com>
2019-08-31 15:50:35 +0530

    release apk bundle 1.15 added

--- app/src/main/java/in/edu/ssn/ssnapp/AboutTeamActivity.java
+++ app/src/main/java/in/edu/ssn/ssnapp/AboutTeamActivity.java
@@ -3,6 +3,8 @@ package in.edu.ssn.ssnapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
