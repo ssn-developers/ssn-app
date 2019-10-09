e25767fe83d306479a8dc137fb29316cc8cecdc1
shrikanth7698 <catcalm7698@gmail.com>
2019-10-09 13:11:56 +0530

    Base activity font bug fixed

--- app/src/main/java/in/edu/ssn/ssnapp/BaseActivity.java
+++ app/src/main/java/in/edu/ssn/ssnapp/BaseActivity.java
@@ -20,7 +20,7 @@ import in.edu.ssn.ssnapp.utils.SharedPref;
 public class BaseActivity extends AppCompatActivity {
 
     //Fonts
-    public Typeface regular, bold, semi_bold;
+    //public Typeface regular, bold, semi_bold;
     public boolean darkModeEnabled = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +34,9 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     private void initFonts(){
-        regular = ResourcesCompat.getFont(this, R.font.open_sans);
-        bold = ResourcesCompat.getFont(this, R.font.open_sans_bold);
-        semi_bold = ResourcesCompat.getFont(this, R.font.open_sans_semi_bold);
+        //regular = ResourcesCompat.getFont(this, R.font.open_sans);
+        //bold = ResourcesCompat.getFont(this, R.font.open_sans_bold);
+        //semi_bold = ResourcesCompat.getFont(this, R.font.open_sans_semi_bold);
     }
 
     //This changes font for all the text views in a view group
