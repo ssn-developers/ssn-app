716de685c065f5b2b9904a9fb35a9ed8d0878331
Ezhil <ezhil99mutsun@gmail.com>
2019-12-05 17:48:29 +0530

    settings activity created

---
+++ app/src/main/java/in/edu/ssn/ssnapp/SettingsActivity.java
@@ -0,0 +1,14 @@
+package in.edu.ssn.ssnapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
