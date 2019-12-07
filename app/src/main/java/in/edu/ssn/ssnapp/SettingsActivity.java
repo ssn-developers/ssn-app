b6759058c9bd4aabbb870f4cbc4bf225c2ebb6de
Ezhil <ezhil99mutsun@gmail.com>
2019-12-07 11:20:48 +0530

    settings updated

--- app/src/main/java/in/edu/ssn/ssnapp/SettingsActivity.java
+++ app/src/main/java/in/edu/ssn/ssnapp/SettingsActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
