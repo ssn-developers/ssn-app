e79d018249b52f27b09ecce3dc4fc1f4ff2d9f66
Logesh Dinakaran <ddlogesh@gmail.com>
2019-12-24 21:11:52 +0530

    code review for grade calc done

--- app/src/main/java/in/edu/ssn/ssnapp/GradeCalculatorActivity.java
+++ app/src/main/java/in/edu/ssn/ssnapp/GradeCalculatorActivity.java
@@ -12,8 +12,8 @@ import spencerstudios.com.bungeelib.Bungee;
 
 public class GradeCalculatorActivity extends BaseActivity {
 
-    TextView OTV,APTV,ATV,BPTV,BTV,internalsTV;
-    ImageView right, left,back;
+    TextView OTV, APTV, ATV, BPTV, BTV, internalsTV;
+    ImageView right, left, back;
     ArrayList<Integer> values = new ArrayList<>();
     int internals;
 
@@ -21,12 +21,11 @@ public class GradeCalculatorActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(darkModeEnabled){
-            setContentView(R.layout.activity_gradecalculator_dark);
+        if (darkModeEnabled) {
+            setContentView(R.layout.activity_grade_calculator_dark);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
                 getWindow().setStatusBarColor(getResources().getColor(R.color.darkColorLight));
-        }
-        else
+        } else
             setContentView(R.layout.activity_grade_calculator);
 
         initUI();
@@ -35,7 +34,7 @@ public class GradeCalculatorActivity extends BaseActivity {
             public void onClick(View view) {
                 internals = Integer.parseInt(internalsTV.getText().toString());
 
-                if(internals<40) {
+                if (internals < 40) {
                     internalsTV.setText(Integer.toString(++internals));
                     getValues(internals);
                 }
@@ -54,7 +53,7 @@ public class GradeCalculatorActivity extends BaseActivity {
             public void onClick(View view) {
                 internals = Integer.parseInt(internalsTV.getText().toString());
 
-                if(internals>0) {
+                if (internals > 0) {
                     internalsTV.setText(Integer.toString(--internals));
                     getValues(internals);
                 }
@@ -72,75 +71,62 @@ public class GradeCalculatorActivity extends BaseActivity {
         right = findViewById(R.id.pm_right_button);
         left = findViewById(R.id.pm_left_button);
         back = findViewById(R.id.backIV);
-        getValues(20);
+        getValues(35);
     }
 
     private void getValues(int mark) {
         values.clear();
-        values.add(0,0);
-        values.add(1,0);
-        values.add(2,0);
-        values.add(3,0);
-        values.add(4,0);
+        values.add(0, 0);
+        values.add(1, 0);
+        values.add(2, 0);
+        values.add(3, 0);
+        values.add(4, 0);
 
-        for(int i=100; i>49; i--) {
-            int n= (int)(0.6 * i)+(mark);
-            if(n>=91)
-            {
+        for (int i = 100; i > 49; i--) {
+            int n = (int) (0.6 * i) + (mark);
+            if (n >= 91) {
                 values.remove(0);
-                values.add(0,i);
-            }else if(n>=81)
-            {
+                values.add(0, i);
+            } else if (n >= 81) {
                 values.remove(1);
-                values.add(1,i);
-            }else if(n>=71)
-            {
+                values.add(1, i);
+            } else if (n >= 71) {
                 values.remove(2);
-                values.add(2,i);
-            }else if(n>=61)
-            {
+                values.add(2, i);
+            } else if (n >= 61) {
                 values.remove(3);
-                values.add(3,i);
-            }else if(n>=50)
-            {
+                values.add(3, i);
+            } else if (n >= 50) {
                 values.remove(4);
-                values.add(4,i);
+                values.add(4, i);
             }
         }
-        if(values.get(0)==0)
-        {
+        if (values.get(0) == 0) {
             OTV.setText("N/A");
-        }else
-        {
+        } else {
             OTV.setText(values.get(0).toString());
         }
 
-        if(values.get(1)==0)
-        {
+        if (values.get(1) == 0) {
             APTV.setText("N/A");
-        }else {
+        } else {
             APTV.setText(values.get(1).toString());
         }
-        if(values.get(2)==0)
-        {
+        if (values.get(2) == 0) {
             ATV.setText("N/A");
-        }else {
+        } else {
             ATV.setText(values.get(2).toString());
         }
-        if(values.get(3)==0)
-        {
+        if (values.get(3) == 0) {
             BPTV.setText("N/A");
-        }else {
+        } else {
             BPTV.setText(values.get(3).toString());
         }
-        if(values.get(4)==0)
-        {
+        if (values.get(4) == 0) {
             BTV.setText("N/A");
-        }else {
+        } else {
             BTV.setText(values.get(4).toString());
         }
-
-
     }
 
     /********************************************************/
@@ -149,5 +135,6 @@ public class GradeCalculatorActivity extends BaseActivity {
     public void onBackPressed() {
         super.onBackPressed();
         Bungee.slideRight(GradeCalculatorActivity.this);
+        finish();
     }
 }
